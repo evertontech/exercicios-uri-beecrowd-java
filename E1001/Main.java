f656125c359ebe7321e17f2e12731c646799109c
Everton Santos <everton_projeto@outlook.com>
2024-01-02 15:04:55 -0300

    exercicio 1001

---
+++ E1001/Main.java
@@ -0,0 +1,13 @@
+package E1001;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int x = a + b;
+        System.out.println("X = " + x);
+    }
+}
