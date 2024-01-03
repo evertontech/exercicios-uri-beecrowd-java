9fa69fcfcd28af744fa29f375aaf27dbb013b048
Everton Santos <everton_projeto@outlook.com>
2024-01-03 15:26:07 -0300

    exercicio 1010

--- E1010/Main.java
+++ E1010/Main.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+        int numeroDePeca1 = scanner.nextInt();
+        float valorDaPeca1 = scanner.nextFloat();
+        scanner.nextInt();
+        int numeroDePeca2 = scanner.nextInt();
+        float valorDaPeca2 = scanner.nextFloat();
+
+        float valorTotal = numeroDePeca1 * valorDaPeca1 + numeroDePeca2 * valorDaPeca2;
 
 
-        System.out.println("");
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", valorTotal);
     }
 }
