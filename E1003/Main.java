f3e51c2d9c86253dc47b67bd0c7f4e2b7480a2d4
Everton Santos <everton_projeto@outlook.com>
2024-01-03 14:19:55 -0300

    exercicio 1003

---
+++ E1003/Main.java
@@ -0,0 +1,13 @@
+package E1003;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int soma = a + b;
+        System.out.println("SOMA = " + soma);
+    }
+}
