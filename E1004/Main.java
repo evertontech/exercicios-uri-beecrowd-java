cb002a37d80e8af2578c3bb84e0997f55aca58c9
Everton Santos <everton_projeto@outlook.com>
2024-01-03 14:20:40 -0300

    exercicio 1004

---
+++ E1004/Main.java
@@ -0,0 +1,14 @@
+package E1004;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int produto = a * b;
+
+        System.out.println("PROD = " + produto);
+    }
+}
