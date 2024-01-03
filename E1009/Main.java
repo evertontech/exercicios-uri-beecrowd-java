c5c1fe00edfb1339a1d0d0f3f3ddc9b2db43fd66
Everton Santos <everton_projeto@outlook.com>
2024-01-03 15:25:51 -0300

    exercicio 1009

--- E1009/Main.java
+++ E1009/Main.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        double salarioFixo = scanner.nextDouble();
+        double vendasTotais = scanner.nextDouble();
+        double comissao = vendasTotais * 0.15;
+        double salarioTotal = salarioFixo + comissao;
 
-
-        System.out.println("");
+        System.out.printf("TOTAL = R$ %.2f\n", salarioTotal);
     }
 }
