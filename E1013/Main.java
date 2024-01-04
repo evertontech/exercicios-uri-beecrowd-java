3116a9a19f38d2923962f6c9d239bff4232414a6
Everton Santos <everton_projeto@outlook.com>
2024-01-04 15:35:32 -0300

    exercicio 1013

--- E1013/Main.java
+++ E1013/Main.java
@@ -1,4 +1,17 @@
 package E1013;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int maiorAB = (a + b + Math.abs(a - b)) / 2;
+        int maior = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+
+        System.out.println(maior + " eh o maior");
+    }
 }
