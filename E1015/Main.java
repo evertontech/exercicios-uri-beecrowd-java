e180f637b686ba108cf8268c32ce72e5278d4f5e
Everton Santos <everton_projeto@outlook.com>
2024-01-04 14:26:27 -0300

    exercicio 1015

--- E1015/Main.java
+++ E1015/Main.java
@@ -6,20 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
         double x2 = scanner.nextDouble();
-        double x3 = scanner.nextDouble();
-        double x4 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
 
         double diferenca1 = x2 - x1;
-        double diferenca2 = x4 - x3;
-        double distanciaX1X2 = Math.pow(diferenca1, 2);
-        double distanciaX3X4 = Math.pow(diferenca2, 2);
-        double distanciaComRaiz = distanciaX1X2 + distanciaX3X4;
-        double distancia = Math.sqrt(distanciaComRaiz);
+        double diferenca2 = y2 - y1;
+        double quadrado1 = Math.pow(diferenca1, 2);
+        double quadrado2 = Math.pow(diferenca2, 2);
+        double soma = quadrado1 + quadrado2;
+        double distancia = Math.sqrt(soma);
 
-        System.out.printf("%.5f\n", distancia);
-//        System.out.println(distanciaX1X2);
-//        System.out.println(distanciaX3X4);
-//        System.out.println(distanciaComRaiz);
+        System.out.printf("%.4f\n", distancia);
     }
 }
