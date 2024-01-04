6168e3459f147f8300c9d27e20eeae65d07d2e7f
Everton Santos <everton_projeto@outlook.com>
2024-01-04 14:26:50 -0300

    exercicio 1035

--- E1035/Main.java
+++ E1035/Main.java
@@ -12,11 +12,8 @@ public class Main {
 
         if ((b > c) && (d > a) && (c + d) > (a + b) && (c > 0) && (d > 0) && (a % 2 == 0)) {
             System.out.println("Valores aceitos");
-
         } else {
             System.out.println("Valores nao aceitos");
         }
-
     }
-
 }
