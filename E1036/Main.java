577e67d0fe603c0e814a9b6cc197989a8a6afe04
Everton Santos <everton_projeto@outlook.com>
2024-01-05 20:46:23 -0300

    exercicio 1036

--- E1036/Main.java
+++ E1036/Main.java
@@ -1,7 +1,23 @@
 package E1036;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double quadradoDeB = Math.pow(b, 2);
+        double delta = quadradoDeB - 4 * a * c;
+        double raizDelta = Math.sqrt(delta);
+        if (a == 0 || delta < 0){
+            System.out.println("Impossivel calcular");
+        } else {
+            double r1 = (- b + raizDelta) / (2 * a);
+            double r2 = (- b - raizDelta) / (2 * a);
+            System.out.printf("R1 = %.5f\n", r1);
+            System.out.printf("R2 = %.5f\n", r2);
+        }
     }
 }
