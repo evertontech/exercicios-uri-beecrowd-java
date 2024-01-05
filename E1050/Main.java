b66d9a2bb728211248b7521cd357147a49610765
Everton Santos <everton_projeto@outlook.com>
2024-01-05 12:43:49 -0300

    exercicio 1050

--- E1050/Main.java
+++ E1050/Main.java
@@ -1,4 +1,30 @@
 package E1050;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ddd = scanner.nextInt();
+
+        if (ddd == 11) {
+            System.out.println("Sao Paulo");
+        } else if (ddd == 27) {
+            System.out.println("Vitoria");
+        } else if (ddd == 31) {
+            System.out.println("Belo Horizonte");
+        } else if (ddd == 32) {
+            System.out.println("Juiz de Fora");
+        } else if (ddd == 61) {
+            System.out.println("Brasilia");
+        } else if (ddd == 71) {
+            System.out.println("Salvador");
+        } else if (ddd == 19) {
+            System.out.println("Campinas");
+        } else if (ddd == 21) {
+            System.out.println("Rio de Janeiro");
+        } else {
+            System.out.println("DDD nao cadastrado");
+        }
+    }
 }
