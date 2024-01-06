df6be756368ab4e33dab799821bdb5148307674b
Everton Santos <everton_projeto@outlook.com>
2024-01-06 18:37:56 -0300

    exercicio 1059

---
+++ E1059/Main.java
@@ -0,0 +1,13 @@
+package E1059;
+
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 100; ++i) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
