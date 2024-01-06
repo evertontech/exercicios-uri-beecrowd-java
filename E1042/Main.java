c1dc3d41eadc2148879bc819865770c13daf4393
Everton Santos <everton_projeto@outlook.com>
2024-01-06 03:00:16 -0300

    exercicio 1042

--- E1042/Main.java
+++ E1042/Main.java
@@ -1,4 +1,68 @@
 package E1042;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a > b) {
+            if (a > c) {
+                if (b > c){
+                    System.out.println(c);
+                    System.out.println(b);
+                    System.out.println(a);
+                    System.out.println();
+                    System.out.println(a);
+                    System.out.println(b);
+                    System.out.println(c);
+                } else {
+                    System.out.println(b);
+                    System.out.println(c);
+                    System.out.println(a);
+                    System.out.println();
+                    System.out.println(a);
+                    System.out.println(b);
+                    System.out.println(c);
+                }
+            } else {
+                System.out.println(b);
+                System.out.println(a);
+                System.out.println(c);
+                System.out.println();
+                System.out.println(a);
+                System.out.println(b);
+                System.out.println(c);
+            }
+        } else if (b > c) {
+                if (a > c) {
+                    System.out.println(c);
+                    System.out.println(a);
+                    System.out.println(b);
+                    System.out.println();
+                    System.out.println(a);
+                    System.out.println(b);
+                    System.out.println(c);
+                } else {
+                    System.out.println(a);
+                    System.out.println(c);
+                    System.out.println(b);
+                    System.out.println();
+                    System.out.println(a);
+                    System.out.println(b);
+                    System.out.println(c);
+                }
+        } else {
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+    }
 }
