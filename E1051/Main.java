a114d58f0809af7c7aacc0d12696e8f3b634073f
Everton Santos <everton_projeto@outlook.com>
2024-01-06 20:26:07 -0300

    exercicio 1051

--- E1051/Main.java
+++ E1051/Main.java
@@ -1,4 +1,16 @@
 package E1051;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float salario = scanner.nextFloat();
+        float valorImposto;
+
+        if (salario >= 2000.00) {
+
+        }
+
+    }
 }
