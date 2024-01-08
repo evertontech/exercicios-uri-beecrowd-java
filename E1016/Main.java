cc4b8eccfede47eb196ff38932bd299724933170
Everton Santos <everton_projeto@outlook.com>
2024-01-08 13:19:16 -0300

    exercicio 1016

--- E1016/Main.java
+++ E1016/Main.java
@@ -1,4 +1,13 @@
 package E1016;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int distanciaDeKm = scanner.nextInt();
+        int tempoNecessario = distanciaDeKm * 2;
+
+        System.out.printf("%d minutos\n", tempoNecessario);
+    }
 }
