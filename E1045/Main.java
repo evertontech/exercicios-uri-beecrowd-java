10997453ef91425789c4401539c9dfa9f8c841b1
Everton Santos <everton_projeto@outlook.com>
2024-01-08 13:20:06 -0300

    exercicio 1045

--- E1045/Main.java
+++ E1045/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float a = scanner.nextFloat();
-        float b = scanner.nextFloat();
-        float c = scanner.nextFloat();
-        float ladoA;
-        float ladoB;
-        float ladoC;
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double ladoA;
+        double ladoB;
+        double ladoC;
 
         if (a > b) {
             if (a > c) {
@@ -32,14 +32,13 @@ public class Main {
                 // c > a > b
             }
         } else if (b > c) {
-                if (a > c) {
-                    ladoA = b;
-                    ladoB = a;
+            ladoA = b;
+            if (a > c) {
+                ladoB = a;
                     ladoC = c;
                     // b > a > c
                 } else {
-                    ladoA = b;
-                    ladoB = c;
+                ladoB = c;
                     ladoC = a;
                     // b > c > a
                 }
@@ -54,18 +53,40 @@ public class Main {
         double quadradoLadoB = Math.pow(ladoB, 2);
         double quadradoLadoC = Math.pow(ladoC, 2);
 
+        int contadorIsosceles = 0;
+        int contadorEquilatero = 0;
+
+        if ((ladoA == ladoB) && (ladoA == ladoC)) {
+            contadorEquilatero = 1;
+        } else if ((ladoA == ladoB) || (ladoA == ladoC)) {
+            contadorIsosceles = 1;
+        }
+
+
         if (ladoA >= (ladoB + ladoC)) {
             System.out.println("NAO FORMA TRIANGULO");
-        } else if ((quadradoLadoA) == (quadradoLadoB + quadradoLadoC)){
-            System.out.println("TRIANGULO RETANGULO");
-        } else if ((quadradoLadoA) > (quadradoLadoB + quadradoLadoC) && ((ladoA > ladoB) && (ladoB > ladoC))) {
-            System.out.println("TRIANGULO OBTUSANGULO");
-        } else if ((quadradoLadoA) < (quadradoLadoB + quadradoLadoC)) {
-            System.out.println("TRIANGULO ACUTANGULO");
         } else if ((ladoA == ladoB) && (ladoB == ladoC)) {
             System.out.println("TRIANGULO EQUILATERO");
         } else if ((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC) && (quadradoLadoA) <= (quadradoLadoB + quadradoLadoC)) {
             System.out.println("TRIANGULO ISOSCELES");
+        } else if ((quadradoLadoA) == (quadradoLadoB + quadradoLadoC)) {
+            System.out.println("TRIANGULO RETANGULO");
+        } else if ((quadradoLadoA) > (quadradoLadoB + quadradoLadoC)) {
+            System.out.println("TRIANGULO OBTUSANGULO");
+            if (contadorIsosceles == 1) {
+                System.out.println("TRIANGULO OBTUSANGULO");
+                System.out.println("TRIANGULO ISOSCELES");
+            }
+        } else if ((quadradoLadoA) < (quadradoLadoB + quadradoLadoC)) {
+            if (contadorIsosceles == 1) {
+                System.out.println("TRIANGULO ACUTANGULO");
+                System.out.println("TRIANGULO ISOSCELES");
+            } else if (contadorEquilatero == 1) {
+                System.out.println("TRIANGULO ACUTANGULO");
+                System.out.println("TRIANGULO EQUILATERO");
+            } else {
+                System.out.println("TRIANGULO ACUTANGULO");
+            }
         }
     }
 }
