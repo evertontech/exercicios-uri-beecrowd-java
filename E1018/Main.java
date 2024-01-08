08765af24d7f25726034a3abb0985c8850c7c16c
Everton Santos <everton_projeto@outlook.com>
2024-01-08 14:55:28 -0300

    exercicio 1018

--- E1018/Main.java
+++ E1018/Main.java
@@ -1,4 +1,19 @@
 package E1018;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int valor = new Scanner(System.in).nextInt();
+        int quantidadeDeNotas;
+        int[] valoresDeNotas = {100, 50, 20, 10, 5, 2, 1};
+
+        System.out.println(valor);
+
+        for (int i = 0; i < 7; ++i){
+            quantidadeDeNotas = valor / valoresDeNotas[i];
+            valor = valor - (quantidadeDeNotas * valoresDeNotas[i]);
+            System.out.printf("%d nota(s) de R$ %d,00\n", quantidadeDeNotas, valoresDeNotas[i]);
+        }
+    }
 }
