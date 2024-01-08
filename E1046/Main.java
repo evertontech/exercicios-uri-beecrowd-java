337cba5646b02b15d560baf23bf7d65a63e5106c
Everton Santos <everton_projeto@outlook.com>
2024-01-08 13:20:13 -0300

    exercicio 1046

--- E1046/Main.java
+++ E1046/Main.java
@@ -1,7 +1,41 @@
 package E1046;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        int horaInicial = scanner.nextInt();
+        int horaFinal = scanner.nextInt();
+        int i;
+        int contadorInicial = 0;
+        int contadorFinal = 0;
+
+        if (horaInicial < 12) {
+            if (horaFinal == 0) {
+                for (i = 0; i <= 24; ++i) {
+                    contadorFinal = i - horaInicial; // 0 - 0
+                }
+                System.out.printf("O JOGO DUROU %d HORA(S)\n", contadorFinal);
+
+            } else {
+                for (i = 0; i <= horaFinal; ++i) {
+                    contadorFinal = i - horaInicial; // 0 - 0
+                }
+                System.out.printf("O JOGO DUROU %d HORA(S)\n", contadorFinal);
+            }
+
+        } else if (horaInicial > 12) {
+            for (i = 0; i <= horaInicial; ++i) {
+                contadorInicial = i; // 16
+            }
+
+            for (i = 0; i <= horaFinal; ++i) {
+                contadorFinal = 24 - i; // 24 - 2 = 22
+            }
+
+            int duracaoJogo = (24 - contadorInicial) + (24 - contadorFinal); // 24 - 16 + 24 - 22
+            System.out.printf("O JOGO DUROU %d HORA(S)\n", duracaoJogo);
+        }
     }
 }
