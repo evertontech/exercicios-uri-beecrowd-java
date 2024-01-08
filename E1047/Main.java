3d174f0838fd21c44e92553013e33253185fc625
Everton Santos <everton_projeto@outlook.com>
2024-01-08 13:20:21 -0300

    exercicio 1047

--- E1047/Main.java
+++ E1047/Main.java
@@ -1,7 +1,34 @@
 package E1047;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        int horaInicial = scanner.nextInt();
+        int horaFinal = scanner.nextInt();
+        int minutoInicial = scanner.nextInt();
+        int minutoFinal = scanner.nextInt();
+        int totalHorasInicial;
+        int totalHorasFinais;
+        int totalMinutos;
+        int totalHoras;
+        int resto;
+
+//        if (horaInicial == horaFinal) {
+//
+//        }
+
+        totalHorasInicial = (horaInicial * 60) + minutoInicial;
+        totalHorasFinais = (horaFinal * 60) + minutoFinal;
+        totalMinutos = totalHorasFinais - totalHorasInicial;
+//        resto = totalMinutos / 60; // 122 % 60
+//        totalHoras = (totalMinutos - resto) / 60;
+        System.out.println(totalMinutos);
+        //System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", totalHoras, totalMinutos);
+
+
+
     }
 }
+
