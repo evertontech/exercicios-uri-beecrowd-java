19c62dbf32efb1d260ba2b5e02204fb4492e3fc5
Everton Santos <everton_projeto@outlook.com>
2024-01-09 13:00:53 -0300

    exercicio 1040

--- E1040/Main.java
+++ E1040/Main.java
@@ -12,25 +12,24 @@ public class Main {
 
         float media = (a * 2 + b * 3 + c * 4 + d * 1) / 10;
 
+        System.out.printf("Media: %.1f\n", media);
+
         if (media >= 7.0) {
-            System.out.printf("Media: %.1f\n", media);
             System.out.printf("Aluno aprovado.\n");
         } else if (media < 5) {
-            System.out.printf("Media: %.1f\n", media);
             System.out.printf("Aluno reprovado.\n");
-        } else if (media >= 5.0 || media <= 6.9){
-            System.out.printf("Media: %.1f\n", media);
+        } else {
             System.out.printf("Aluno em exame.\n");
             float notaExame = scanner.nextFloat();
             System.out.printf("Nota do exame: %.1f\n", notaExame);
-            float exame = (notaExame + media) / 2;
-            if (exame >= 5.0) {
+            float mediaFinal = (notaExame + media) / 2;
+            if (mediaFinal >= 5.0) {
                 System.out.printf("Aluno aprovado.\n");
-                System.out.printf("Media final: %.1f\n", exame);
             } else {
                 System.out.printf("Aluno reprovado.\n");
-                System.out.printf("Media final: %.1f\n", exame);
             }
+
+            System.out.printf("Media final: %.1f\n", mediaFinal);
         }
 
 
