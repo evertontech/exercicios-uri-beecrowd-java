fe1cfb0409dfc3b0dd9599c2fbf32479000cbb76
Everton Santos <everton_projeto@outlook.com>
2024-01-09 13:00:21 -0300

    exercicio 1019

--- E1019/Main.java
+++ E1019/Main.java
@@ -4,22 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int valor = scanner.nextInt();
-        int horas;
-        int minutos;
-        int restoSegundos;
-        if (valor > 3600) {
-            horas = valor / 3600; // 140153 / 3600 = 38
-            minutos = (valor - (3600 * horas)) / 60; // 140153 - 3600 * 38 = 3353 / 60 = 55
-            restoSegundos = valor - (3600 * horas) - (60 * minutos);
-            System.out.printf("%d:%d:%d\n", horas, minutos, restoSegundos);
-        } else if (valor > 60) {
-            minutos = valor / 60; // 65 / 60 = 1
-            restoSegundos = valor - (60 * minutos); // 65 - 60 * 1 = 5
-            System.out.printf("0:%d:%d\n", minutos, restoSegundos);
-        } else {
-            System.out.printf("0:0:%d\n", valor);
-        }
+        int valor = new Scanner(System.in).nextInt();
+
+        int horas = valor / 3600;
+        valor -= horas * 3600;
+
+        int minutos = valor / 60;
+        valor -= minutos * 60;
+
+        int segundos = valor;
+
+        System.out.printf("%d:%d:%d\n", horas, minutos, segundos);
+
     }
 }
