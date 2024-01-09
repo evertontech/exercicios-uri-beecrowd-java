4ab23db98cde9428434c85677ccd58be06c792a1
Everton Santos <everton_projeto@outlook.com>
2024-01-09 00:49:51 -0300

    exercicio 1075

---
+++ E1075/Main.java
@@ -0,0 +1,16 @@
+package E1075;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10000; ++i) {
+            if (i % n == 2) {
+                System.out.println(i);
+            }
+        }
+    }
+}
