9c81903250fa37fd678566bd0adb54bb9a78e622
Everton Santos <everton_projeto@outlook.com>
2024-01-22 02:06:51 -0300

    exercicio 1142

---
+++ E1142/Main.java
@@ -0,0 +1,14 @@
+package E1142;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int n = scanner.nextInt();
+
+        for (int i = 1; i <= n * 4; i = i + 4) {
+            System.out.printf("%d %d %d PUM\n", i, i + 1, i + 2);
+        }
+    }
+}
