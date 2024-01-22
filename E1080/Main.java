5da3d25edff1f2aa6ca129a189bf964f16fb5abd
Everton Santos <everton_projeto@outlook.com>
2024-01-22 02:07:45 -0300

    exercicio 1080

---
+++ E1080/Main.java
@@ -0,0 +1,22 @@
+package E1080;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        int maior = 0;
+        int pos = 0;
+
+        for (int i = 1; i <= 100; ++i) {
+            n = scanner.nextInt();
+            if (n > maior) {
+                maior = n;
+                pos = i;
+            }
+        }
+
+        System.out.println(maior);
+        System.out.println(pos);
+    }
+}
