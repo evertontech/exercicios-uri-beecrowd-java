4f00408a1a3a5e2d0be98f0496c3ad8bc0f5839f
Everton Santos <everton_projeto@outlook.com>
2024-01-23 14:26:56 -0300

    exercicio 1067

--- E1067/Main.java
+++ E1067/Main.java
@@ -4,13 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int i;
-        for (i = 1; i <= x; ++i){
-            if (i % 2 != 0) {
-                System.out.println(i);
-            }
+        int x = new Scanner(System.in).nextInt();
+
+        for (int i = 1; i <= x; i += 2) {
+            System.out.println(i);
         }
     }
 }
