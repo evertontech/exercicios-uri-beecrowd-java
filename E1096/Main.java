622a9ecf31a961b7de55301d8d1df5587426d6da
Everton Santos <everton_projeto@outlook.com>
2024-01-23 15:57:55 -0300

    exercicio 1096

--- E1096/Main.java
+++ E1096/Main.java
@@ -2,10 +2,8 @@ package E1096;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        int j = 0;
 
-        for (i = 1,  j = 7; i <= 9; i = i + 2) {
+        for (int i = 1, j = 7; i <= 9; i = i + 2) {
             System.out.println("I=" + i + " J=" + j);
             System.out.println("I=" + i + " J=" + (j - 1));
             System.out.println("I=" + i + " J=" + (j - 2));
