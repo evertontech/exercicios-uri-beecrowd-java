9f4b98a92df85a69964896f11e301d964d66998c
Everton Santos <everton_projeto@outlook.com>
2024-01-23 14:25:15 -0300

    exercicio 1046

--- E1046/Main.java
+++ E1046/Main.java
@@ -5,37 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int horaInicial = scanner.nextInt();
-        int horaFinal = scanner.nextInt();
-        int i;
-        int contadorInicial = 0;
-        int contadorFinal = 0;
+        int a = scanner.nextInt(); // 2
+        int b = scanner.nextInt(); // 16
+        int tempoDeJogo;
 
-        if (horaInicial < 12) {
-            if (horaFinal == 0) {
-                for (i = 0; i <= 24; ++i) {
-                    contadorFinal = i - horaInicial; // 0 - 0
-                }
-                System.out.printf("O JOGO DUROU %d HORA(S)\n", contadorFinal);
+        if (a < b) {
+            tempoDeJogo = b - a; //  14
 
-            } else {
-                for (i = 0; i <= horaFinal; ++i) {
-                    contadorFinal = i - horaInicial; // 0 - 0
-                }
-                System.out.printf("O JOGO DUROU %d HORA(S)\n", contadorFinal);
-            }
+        } else {
+            tempoDeJogo = 24 - (a - b); // 24 - 16 - 2
 
-        } else if (horaInicial > 12) {
-            for (i = 0; i <= horaInicial; ++i) {
-                contadorInicial = i; // 16
-            }
-
-            for (i = 0; i <= horaFinal; ++i) {
-                contadorFinal = 24 - i; // 24 - 2 = 22
-            }
-
-            int duracaoJogo = (24 - contadorInicial) + (24 - contadorFinal); // 24 - 16 + 24 - 22
-            System.out.printf("O JOGO DUROU %d HORA(S)\n", duracaoJogo);
         }
+
+        System.out.println("O JOGO DUROU " + tempoDeJogo + " HORA(S)");
     }
 }
