bdceebd6b42838c83d89dda1b333ee9c24f5a442
Everton Santos <everton_projeto@outlook.com>
2024-01-23 14:55:11 -0300

    exercicio 1065

--- E1065/Main.java
+++ E1065/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int contador = 0;
 
         for (int i = 1; i <= 5; ++i) {
