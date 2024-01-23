a222d56c1b99684829f3227e39f2c05fd33f0ea6
Everton Santos <everton_projeto@outlook.com>
2024-01-23 15:57:21 -0300

    exercicio 1073

--- E1073/Main.java
+++ E1073/Main.java
@@ -6,15 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int i;
-        //int quadrado;
 
-        for (i = 1; i <= n; ++i) {
-            if (i % 2 == 0) {
-                double quadrado = Math.pow(i, 2);
-                System.out.printf("%d^2 = %.0f\n", i, quadrado);
-            }
+        for (int i = 0; i <= n; i = i + 2) {
+            double quadrado = Math.pow(i, 2);
+            System.out.printf("%d^2 = %.0f\n", i, quadrado);
         }
-
     }
 }
