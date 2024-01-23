324571ab372db3515ef4892d14c634fa6459976a
Everton Santos <everton_projeto@outlook.com>
2024-01-23 14:26:17 -0300

    exercicio 1061

---
+++ E1061/Main.java
@@ -0,0 +1,12 @@
+package E1061;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String dia = "Dia";
+        int dias = new Scanner(System.in);
+
+
+    }
+}
