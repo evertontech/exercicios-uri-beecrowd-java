ff61ba99ae72900a99319116e2b2ae06f57c2b69
Everton Santos <everton_projeto@outlook.com>
2024-01-23 14:26:25 -0300

    exercicio 1064

--- E1064/Main.java
+++ E1064/Main.java
@@ -6,23 +6,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double[] vetorValoresDeEntrada = new double[6];
-        double[] vetorValoresPositivos = new double[6];
         double soma = 0;
-
         int contador = 0;
 
-        for (int i = 0; i < vetorValoresDeEntrada.length; ++i) {
-            vetorValoresDeEntrada[i] = scanner.nextDouble();
-            if (vetorValoresDeEntrada[i] > 0) {
-                soma = soma + vetorValoresDeEntrada[i];
-                contador += 1;
+        for (int i = 1; i <= 6; ++i) {
+            double entrada = scanner.nextDouble();
+
+            if (entrada > 0) {
+                soma += entrada;
+                contador++;
             }
         }
 
         double media = soma / contador;
 
-        System.out.printf("%d valores positivos\n", contador);
+        System.out.println(contador + " valores positivos");
         System.out.printf("%.1f\n", media);
 
     }
