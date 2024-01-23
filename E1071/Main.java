aa18d3799a8941cc498c9f12adc0364fe443e469
Everton Santos <everton_projeto@outlook.com>
2024-01-23 14:55:22 -0300

    exercicio 1071

--- E1071/Main.java
+++ E1071/Main.java
@@ -9,6 +9,12 @@ public class Main {
         int y = scanner.nextInt();
         int somaImpares = 0;
 
+        if (x < y) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
         for (int i = y + 1; i < x; ++i) {
             if (i % 2 != 0) {
                 somaImpares = somaImpares + i;
