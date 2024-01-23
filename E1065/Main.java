635bd5778938713f7cb659ed6846cdbe2388bee0
Everton Santos <everton_projeto@outlook.com>
2024-01-23 14:26:45 -0300

    exercicio 1065

---
+++ E1065/Main.java
@@ -0,0 +1,21 @@
+package E1065;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int contador = 0;
+
+        for (int i = 1; i <= 5; ++i) {
+            int entrada = scanner.nextInt();
+
+            if (entrada % 2 == 0) {
+                contador++;
+            }
+        }
+
+        System.out.println(contador + " valores pares");
+    }
+}
