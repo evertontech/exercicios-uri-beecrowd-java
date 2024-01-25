b1606836f45cbc29d2a8c091e03d3f45e417667d
Everton Santos <everton_projeto@outlook.com>
2024-01-25 16:08:58 -0300

    exercicio 1114

--- E1114/Main.java
+++ E1114/Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int senha = 0;
+        int senha;
 
-        while (senha != 2002) {
+        do {
             senha = scanner.nextInt();
 
             if (senha != 2002) {
                 System.out.println("Senha Invalida");
             }
-        }
+
+        } while (senha != 2002);
+
         System.out.println("Acesso Permitido");
     }
 }
