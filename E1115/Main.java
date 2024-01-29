d4416e504fdaa13e16500896edcc301cf7ce57cf
Everton Santos <everton_projeto@outlook.com>
2024-01-29 01:02:46 -0300

    exercicio 1115

--- E1115/Main.java
+++ E1115/Main.java
@@ -1,7 +1,30 @@
 package E1115;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        do {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            if (x == 0 | y == 0) {
+                break;
+            }
+
+            if (x > 0 && y > 0) {
+                System.out.println("primeiro");
+            } else if (x < 0 && y > 0) {
+                System.out.println("segundo");
+            } else if (x < 0 && y < 0) {
+                System.out.println("terceiro");
+            } else {
+                System.out.println("quarto");
+            }
+        } while (true);
 
     }
 }
