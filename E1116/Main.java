bc69979c0317be44b9c4a8350ae05fba8e84a2c2
Everton Santos <everton_projeto@outlook.com>
2024-01-31 16:05:41 -0300

    exercicio 1116

--- E1116/Main.java
+++ E1116/Main.java
@@ -1,7 +1,24 @@
 package E1116;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
+        for (int i = 0; i < n; ++i) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            float soma = (float) x / y;
+
+            if (y == 0) {
+                System.out.println("divisao impossivel");
+            } else if (x == 0) {
+                System.out.println("0.0");
+            } else {
+                System.out.println(soma);
+            }
+        }
     }
 }
