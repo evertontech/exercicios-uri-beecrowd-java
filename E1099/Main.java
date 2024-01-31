78711480d708506058864afff62f1e591ed0b866
Everton Santos <everton_projeto@outlook.com>
2024-01-31 14:19:19 -0300

    exercicio 1099

--- E1099/Main.java
+++ E1099/Main.java
@@ -6,17 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int soma = 0;
 
         for (int j = 0; j < n; ++j) {
             int x = scanner.nextInt();
             int y = scanner.nextInt();
 
             if (x > y) {
-                int temp = x;
-                x = y;
-                y = temp;
+                int temp = y;
+                y = x;
+                x = temp;
             }
+
+            int soma = 0;
 
             for (int i = x + 1; i < y; ++i) {
                 if (i % 2 != 0) {
@@ -25,7 +26,6 @@ public class Main {
             }
 
             System.out.println(soma);
-            soma = 0;
         }
     }
 }
