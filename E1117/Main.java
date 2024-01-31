5ba688bfb792af13ec221b47edbb4b2f68d7aa3e
Everton Santos <everton_projeto@outlook.com>
2024-01-31 16:05:47 -0300

    exercicio 1117

--- E1117/Main.java
+++ E1117/Main.java
@@ -1,7 +1,26 @@
 package E1117;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int contador = 0;
+        float mediaNotas = 0;
 
+        while(contador != 2) {
+            float nota = scanner.nextFloat();
+
+            if (nota >= 0 && nota <= 10 && contador < 2) {
+                contador++;
+                mediaNotas += nota;
+                if (contador == 2) {
+                    mediaNotas = mediaNotas / 2;
+                    System.out.printf("media = %.2f\n", mediaNotas);
+                }
+            } else {
+                System.out.println("nota invalida");
+            }
+        }
     }
 }
