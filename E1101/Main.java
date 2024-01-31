559de0ef5da360bb94eacc79cf6f2c225af4a207
Everton Santos <everton_projeto@outlook.com>
2024-01-31 14:19:34 -0300

    exercicio 1101

--- E1101/Main.java
+++ E1101/Main.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int soma = 0;
 
         do {
             int m = scanner.nextInt();
             int n = scanner.nextInt();
 
-            if (m == 0 | n == 0 | m < 0 | n < 0) {
+            if (m <= 0 || n <= 0 ) {
                 break;
             }
 
@@ -20,13 +19,15 @@ public class Main {
                 m = n;
                 n = temp;
             }
+
+            int soma = 0;
+
             for (int i = n; i <= m; ++i) {
                 System.out.print(i + " ");
                 soma = soma + i;
             }
 
             System.out.println("Sum=" + soma);
-            soma = 0;
 
         } while (true);
     }
