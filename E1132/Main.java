a01473e4a13f29b278846ac5c3efcba8ddc324d1
Everton Santos <everton_projeto@outlook.com>
2024-01-31 14:19:56 -0300

    exercicio 1132

--- E1132/Main.java
+++ E1132/Main.java
@@ -1,7 +1,26 @@
 package E1132;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int soma = 0;
 
+        if (x < y) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        for (int i = x; i <= y; ++i){
+            if (i % 13 == 0) {
+                soma = soma + i;
+            }
+        }
+
+        System.out.println(soma);
     }
 }
