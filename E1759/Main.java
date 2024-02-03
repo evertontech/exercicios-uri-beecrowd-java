028162cec81035ab0ceb9a6b9db914ab5d81ee35
Everton Santos <everton_projeto@outlook.com>
2024-02-03 10:41:54 -0300

    exercicio 1759

---
+++ E1759/Main.java
@@ -0,0 +1,16 @@
+package E1759;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i < n; ++i) {
+            System.out.print("Ho ");
+        }
+
+        System.out.print("Ho!");
+    }
+}
