0b1ea2244cef4060c59531dee7e58fdd75621dce
Everton Santos <everton_projeto@outlook.com>
2024-02-03 10:44:58 -0300

    exercicio 1172

--- E1172/Main.java
+++ E1172/Main.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] vetorInteiros = new int[10];
 
+        for (int i = 0; i < 10; ++i) {
+            vetorInteiros[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < 10; ++i) {
+            if (vetorInteiros[i] <= 0) {
+                vetorInteiros[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < 10; ++i) {
+            System.out.printf("X[%d] = %d\n", i, vetorInteiros[i]);
+        }
     }
 }
