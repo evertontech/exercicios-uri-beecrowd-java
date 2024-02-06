12566560506524ba5b101c372c5fc92fc38b397e
Everton Santos <everton_projeto@outlook.com>
2024-02-06 15:24:33 -0300

    exercicio 1155

--- E1155/Main.java
+++ E1155/Main.java
@@ -1,13 +1,11 @@
 package E1155;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         float s = 1;
 
         for (int i = 2; i <= 100; ++i) {
-            s = s + (1 / (float) i);
+            s = s + (1.f / i);
         }
 
         System.out.printf("%.2f\n", s);
