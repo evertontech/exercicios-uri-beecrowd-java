9b2dcae617b5810b76d05b752132c0dd2f610575
Everton Santos <everton_projeto@outlook.com>
2024-02-08 15:30:11 -0300

    exercicio 1157

--- E1157/Main.java
+++ E1157/Main.java
@@ -6,12 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int limite = n / 2;
 
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 1; i <= limite; ++i) {
             if (n % i == 0) {
                 System.out.println(i);
             }
         }
 
+        System.out.println(n);
     }
 }
--- E1173/Main.java
+++ E1173/Main.java
@@ -10,12 +10,12 @@ public class Main {
 
         vetorInteiros[0] = x;
 
-        for (int i = 0; i < 9; ++i) {
-            vetorInteiros[i + 1] = vetorInteiros[i] * 2;
+        for (int i = 1; i < 10; ++i) {
+            vetorInteiros[i] = vetorInteiros[i - 1] * 2;
         }
 
         for (int i = 0; i < 10; ++i) {
-            System.out.printf("N[%d] = %d\n", i, vetorInteiros[i]);
+            System.out.println("N[" + i + "] = " +  vetorInteiros[i]);
         }
     }
 }
