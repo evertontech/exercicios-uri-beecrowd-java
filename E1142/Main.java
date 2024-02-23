161ec6ed7237f129fdc5208d3f5573eb5e05f041
Everton Santos <everton_projeto@outlook.com>
2024-02-23 15:20:48 -0300

    exercicio 1142

--- E1142/Main.java
+++ E1142/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       int n = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
         for (int i = 1; i <= n * 4; i = i + 4) {
             System.out.printf("%d %d %d PUM\n", i, i + 1, i + 2);
