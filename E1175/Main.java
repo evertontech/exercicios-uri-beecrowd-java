78917b31afefe4ee264416e23ee1fec0dcba35b9
Everton Santos <everton_projeto@outlook.com>
2024-02-23 15:43:27 -0300

    exercicio 1175

--- E1175/Main.java
+++ E1175/Main.java
@@ -6,18 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] vetorInteiros = new int[20];
-        int[] vetor2 = new int[20];
 
         for (int i = 0; i < 20; ++i) {
             vetorInteiros[i] = scanner.nextInt();
         }
 
-        for (int i = 0; i < 20; ++i) {
-            vetor2[19 - i] = vetorInteiros[i];
+        for (int i = 0; i < 10; ++i) {
+            int temp = vetorInteiros[19 - i];
+            vetorInteiros[19 - i] = vetorInteiros[i];
+            vetorInteiros[i] = temp;
         }
 
         for (int i = 0; i < 20; ++i) {
-            System.out.printf("N[%d] = %d\n", i, vetor2[i]);
+            System.out.printf("N[%d] = %d\n", i, vetorInteiros[i]);
         }
     }
 }
