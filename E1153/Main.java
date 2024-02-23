b9473256c42fdde99af53d38787f91383f21a3c5
Everton Santos <everton_projeto@outlook.com>
2024-02-23 15:21:01 -0300

    exercicio 1153

--- E1153/Main.java
+++ E1153/Main.java
@@ -6,16 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int fatorial = 1;
+        int fatorial = n;
 
-        for (int i = 0; i <= n; ++i) {
-            n = n - i;
-            System.out.println(n);
-            //int multiplicacao = fatorial * n;
-            //System.out.println();
-            System.out.println(fatorial);
+        for (int i = 1; i < n; ++i) {
+            fatorial = fatorial * (n - i);
         }
 
-        //System.out.println(fatorial);
+        System.out.println(fatorial);
     }
 }
