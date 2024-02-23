0f63db6df2b43c8ca41d1dba9a1fc9d9bb787bdc
Everton Santos <everton_projeto@outlook.com>
2024-02-23 15:21:45 -0300

    exercicio 1173

--- E1173/Main.java
+++ E1173/Main.java
@@ -10,12 +10,13 @@ public class Main {
 
         vetorInteiros[0] = x;
 
-        for (int i = 1; i < 10; ++i) {
-            vetorInteiros[i] = vetorInteiros[i - 1] * 2;
+        for (int i = 0; i < 10; ++i) {
+            vetorInteiros[i + 1] = vetorInteiros[i];
+            vetorInteiros[i] = vetorInteiros[i + 1] * 2;
         }
 
         for (int i = 0; i < 10; ++i) {
-            System.out.println("N[" + i + "] = " +  vetorInteiros[i]);
+            System.out.println("N[" + i + "] = " + vetorInteiros[i]);
         }
     }
 }
