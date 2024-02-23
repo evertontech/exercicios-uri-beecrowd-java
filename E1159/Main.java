39db9d131bfc62c30736a116033d319782d43468
Everton Santos <everton_projeto@outlook.com>
2024-02-23 15:21:15 -0300

    exercicio 1159

--- E1159/Main.java
+++ E1159/Main.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int x;
-        int soma = 0;
 
-        do {
-            x = scanner.nextInt();
+        while (true) {
+            int x = scanner.nextInt();
+
+            if (x == 0) {
+                break;
+            }
 
             if (x % 2 != 0) {
                 x++;
             }
 
-            for (int i = 0; i < 5 && x != 0; i++) {
-                soma += x + 2 * i;
+            int soma = 0;
+
+            for (int i = 0; i < 5; i++) {
+                soma += x;
+                x += 2;
             }
 
             System.out.println(soma);
-            soma = 0;
-
-        } while (x != 0);
+        }
     }
 }
