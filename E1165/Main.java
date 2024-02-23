4d5fd2bfcd67199668a870eaaaa107be3dd5710e
Everton Santos <everton_projeto@outlook.com>
2024-02-23 15:21:39 -0300

    exercicio 1165

--- E1165/Main.java
+++ E1165/Main.java
@@ -1,7 +1,30 @@
 package E1165;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
+        for (int i = 0; i < n; ++i) {
+            int x = scanner.nextInt();
+            boolean achouDivisor = false;
+
+            double raizQuadrada = Math.sqrt(x);
+
+            for (int j = 2; j <= raizQuadrada; ++j) {
+                if (x % j == 0) {
+                    achouDivisor = true;
+                    break;
+                }
+            }
+
+            if (achouDivisor) {
+                System.out.println(x + " nao eh primo");
+            } else {
+                System.out.println(x + " eh primo");
+            }
+        }
     }
 }
