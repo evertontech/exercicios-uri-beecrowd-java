67078bc4867aeb01b0bb5f456491166942022540
Everton Santos <everton_projeto@outlook.com>
2024-02-23 15:19:05 -0300

    exercicio 1041

--- E1041/Main.java
+++ E1041/Main.java
@@ -8,15 +8,15 @@ public class Main {
         float x = scanner.nextFloat();
         float y = scanner.nextFloat();
 
-        if (x < 0 && y > 0){
+        if (x < 0 && y > 0) {
             System.out.println("Q2");
-        } else if (x > 0 && y > 0){
+        } else if (x > 0 && y > 0) {
             System.out.println("Q1");
-        } else if (x < 0 && y < 0){
+        } else if (x < 0 && y < 0) {
             System.out.println("Q3");
-        } else if (x > 0 && y < 0){
+        } else if (x > 0 && y < 0) {
             System.out.println("Q4");
-        } else if (x == 0 && y != 0){
+        } else if (x == 0 && y != 0) {
             System.out.println("Eixo Y");
         } else if (y == 0 && x != 0) {
             System.out.println("Eixo X");
