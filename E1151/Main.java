a23c585ae845f46d118281268bb42bca56a98a23
Everton Santos <everton_projeto@outlook.com>
2024-02-27 15:04:37 -0300

    exercicio 1151

--- E1151/Main.java
+++ E1151/Main.java
@@ -7,14 +7,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int anterior = 0;
-        int atual = 0;
+        int atual = 1;
         int proximo;
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; ++i) {
-            proximo = anterior + atual; // 0 + 1, 1 + 1, 1, 2
-            anterior = atual; // 1, 1
-            atual = i; // 0, 1
-            System.out.println(proximo);
+            sb.append(anterior);
+
+            if (i != n - 1) {
+                sb.append(" ");
+            }
+            proximo = anterior + atual;
+            anterior = atual;
+            atual = proximo;
         }
+
+        System.out.println(sb);
     }
 }
