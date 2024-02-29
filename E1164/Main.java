98cbb94f97f7bf7ec6f0896647021cf434cc7d78
Everton Santos <everton_projeto@outlook.com>
2024-02-29 15:33:28 -0300

    exercicio 1164

--- E1164/Main.java
+++ E1164/Main.java
@@ -6,12 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int soma = 0;
 
         for (int i = 0; i < n; ++i) {
             int x = scanner.nextInt();
 
-            for (int j = 1; j < x; ++j) {
+            int soma = 0;
+            int metade = x / 2;
+
+            for (int j = 1; j <= metade; ++j) {
                 if (x % j == 0) {
                     soma += j;
                 }
@@ -19,10 +21,8 @@ public class Main {
 
             if (soma == x) {
                 System.out.println(x + " eh perfeito");
-                soma = 0;
             } else {
                 System.out.println(x + " nao eh perfeito");
-                soma = 0;
             }
         }
     }
