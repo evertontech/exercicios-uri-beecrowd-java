073d21c1a946516fba9b90ced2baac2d20712c2b
Everton Santos <everton_projeto@outlook.com>
2024-02-29 15:32:57 -0300

    exercicio 1179

--- E1179/Main.java
+++ E1179/Main.java
@@ -1,4 +1,42 @@
 package E1179;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        int[] vetorPar = new int[5];
+        int[] vetorImpar = new int[5];
+        int indexPar = 0;
+        int indexImpar = 0;
+
+        for (int i = 0; i < 15; ++i) {
+            x = scanner.nextInt();
+
+            if (x % 2 != 0) {
+                vetorImpar[indexImpar] = x;
+                indexImpar++;
+            } else {
+                vetorPar[indexPar] = x;
+                indexPar++;
+            }
+
+            if (indexImpar == 5 || i == 14) {
+                for (int j = 0; j < indexImpar; ++j) {
+                    System.out.println("impar[" + j + "] = " + vetorImpar[j]);
+                }
+
+                indexImpar = 0;
+            }
+
+            if (indexPar == 5 || i == 14) {
+                for (int j = 0; j < indexPar; ++j) {
+                    System.out.println("par[" + j + "] = " + vetorPar[j]);
+                }
+
+                indexPar = 0;
+            }
+        }
+    }
 }
