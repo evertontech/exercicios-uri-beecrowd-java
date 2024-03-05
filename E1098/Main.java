812d524f523e0ff0ee5b871980048c06e3b2bb83
Everton Santos <everton_projeto@outlook.com>
2024-03-05 14:45:39 -0300

    exercicio 1098

--- E1098/Main.java
+++ E1098/Main.java
@@ -1,19 +1,30 @@
 package E1098;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+        BigDecimal zeroPontoDois = new BigDecimal("0.2");
+        BigDecimal zero = new BigDecimal("0");
+        BigDecimal um = new BigDecimal("1");
+        BigDecimal dois = new BigDecimal("2");
+        BigDecimal tres = new BigDecimal("3");
 
-        for (double i = 0; i <= 2; i += 0.2) {
-            for (double j = 1; j <= 3; ++j) {
-                if ((int) i == i) {
-                    if ((int) j == j) {
-                        System.out.printf("I=%.0f J=%.0f\n", i, j);
-                    } else {
-                        System.out.printf("I=%.0f J=%.1f\n", i, j);
-                    }
+        for (BigDecimal i = zero; i.compareTo(dois) <= 0; i = i.add(zeroPontoDois)) {
+            for (BigDecimal j = um; j.compareTo(tres) <= 0; j = j.add(um)) {
 
+                if (i.stripTrailingZeros().scale() == 0) {
+                    System.out.printf("I=%.0f ", i);
                 } else {
-                    System.out.printf("I=%.1f J=%.1f\n", i, j + i);
+                    System.out.printf("I=%.1f ", i);
+                }
+
+                BigDecimal soma = i.add(j);
+
+                if (soma.stripTrailingZeros().scale() == 0) {
+                    System.out.printf("J=%.0f\n", soma);
+                } else {
+                    System.out.printf("J=%.1f\n", soma);
                 }
             }
         }
