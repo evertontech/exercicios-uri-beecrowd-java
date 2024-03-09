f89a3db3d901ba69aa70c4685d9f6cffb3daef99
Everton Santos <everton_projeto@outlook.com>
2024-03-08 21:58:04 -0300

    exercicio 1184

--- E1184/Main.java
+++ E1184/Main.java
@@ -1,35 +1,32 @@
 package E1184;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String t = scanner.nextLine();
-        scanner.nextLine();
-        float valores = scanner.nextFloat();
         float[][] matriz = new float[12][12];
         float soma = 0.0f;
-        float media = 0.0f;
+
+        String t = scanner.nextLine();
+
+        int contador = 0;
 
         for (int i = 0; i < 12; ++i) {
             for (int j = 0; j < 12; ++j) {
-                matriz[i][j] = valores;
-            }
-        }
+                matriz[i][j] = scanner.nextFloat();
 
-        for (int i = 0; i < 12; ++i) {
-            for (int j = 0; j < 12; ++j) {
-                soma += matriz[i][j];
-                media += matriz[i][j];
+                if (i > j) {
+                    soma += matriz[i][j];
+                    contador++;
+                }
             }
         }
 
         if (t.equals("S")) {
             System.out.printf("%.1f\n", soma);
         } else if (t.equals("M")) {
-            System.out.printf("%.1f\n", media);
+            System.out.printf("%.1f\n", soma / contador);
         }
     }
 }
