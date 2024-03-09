3aa7c9b8fc353ee6d3d6fc8f6fa59c03d9dabb7e
Everton Santos <everton_projeto@outlook.com>
2024-03-08 21:57:15 -0300

    exercicio 1185

--- E1185/Main.java
+++ E1185/Main.java
@@ -1,4 +1,38 @@
 package E1185;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float[][] matriz = new float[12][12];
+        float soma = 0.0f;
+
+        String t = scanner.nextLine();
+        String linhaBranco = scanner.nextLine();
+
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 12; ++j) {
+                matriz[i][j] = scanner.nextFloat();
+            }
+        }
+
+        int contador = 0;
+
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 12; ++j) {
+                if ((i + j) < 5) {
+                    soma += matriz[i][j];
+                    contador++;
+                }
+            }
+        }
+
+        if (t.equals("S")) {
+            System.out.printf("%.1f\n", soma);
+        } else if (t.equals("M")) {
+
+            System.out.printf("%.1f\n", soma / contador);
+        }
+    }
 }
