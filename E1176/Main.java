0dbb224b8f688ea0d978960032b10f25f5ae4d98
Everton Santos <everton_projeto@outlook.com>
2024-03-08 21:56:41 -0300

    exercicio 1176

--- E1176/Main.java
+++ E1176/Main.java
@@ -1,7 +1,23 @@
 package E1176;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long[] vetor = new long[61];
+        int t = scanner.nextInt();
 
+        vetor[0] = 0;
+        vetor[1] = 1;
+
+        for (int i = 2; i < 61; ++i) {
+            vetor[i] = vetor[i - 2] + vetor[i - 1];
+        }
+
+        for (int i = 0; i < t; ++i) {
+            int n = scanner.nextInt();
+            System.out.println("Fib(" + n + ") = " + vetor[n]);
+        }
     }
 }
