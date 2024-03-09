1346bb9d72679fca7792c46c16f56e3b960d2a3e
Everton Santos <everton_projeto@outlook.com>
2024-03-08 21:57:42 -0300

    exercicio 1182

--- E1182/Main.java
+++ E1182/Main.java
@@ -5,30 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        float[][] matriz = new float[12][12];
+        float soma = 0.0f;
+
         int c = scanner.nextInt();
         scanner.nextLine();
         String t = scanner.nextLine();
-        float[][] matriz = new float[12][12];
-        float soma = 0.0f;
-        float media = 0.0f;
 
         for (int i = 0; i < 12; ++i) {
             for (int j = 0; j < 12; ++j) {
-                float x = scanner.nextFloat();
-                matriz[i][j] = x;
+                matriz[i][j] = scanner.nextFloat();
             }
         }
 
         for (int i = 0; i < 12; ++i) {
             soma += matriz[i][c];
-            media += matriz[i][c];
         }
 
         if (t.equals("S")) {
             System.out.printf("%.1f\n", soma);
         } else if (t.equals("M")) {
-            media = media / 12;
-            System.out.printf("%.1f\n", media);
+            System.out.printf("%.1f\n", soma / 12);
         }
     }
 }
