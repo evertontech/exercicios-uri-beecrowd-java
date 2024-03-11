f43fcd1b2b206a8e50d823c1f23dab777b4f19bd
Everton Santos <everton_projeto@outlook.com>
2024-03-11 16:07:35 -0300

    exercicio 1847

---
+++ E1847/E1847.java
@@ -0,0 +1,44 @@
+package E1847;
+
+import java.util.Scanner;
+
+public class E1847 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a > b && b <= c) { // ok
+            System.out.println(":)");
+        }
+
+        if (b > a && b >= c) { // ok
+            System.out.println(":(");
+        }
+
+        if (b > a && c > b && c - b < b - a) { // ok
+            System.out.println(":(");
+        }
+
+        if (b > a && c > b && (c - b) >= (b - a)) { // ok
+            System.out.println(":)");
+        }
+
+        if (a > b && b > c && (b - c) < (a - b)) { // ok
+            System.out.println(":)");
+        }
+
+        if (a > b && b > c && (b - c) >= (a - b)) {
+            System.out.println(":(");
+        }
+
+        if (a == b && c > b) { // ok
+            System.out.println(":)");
+        }
+
+        if (a == b && b > c) { // ok
+            System.out.println(":(");
+        }
+    }
+}
