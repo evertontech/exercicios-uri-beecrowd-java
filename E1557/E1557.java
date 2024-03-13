0ab87e535c4d3ce37efbc07cc9c94b135b250616
Everton Santos <everton_projeto@outlook.com>
2024-03-13 16:57:42 -0300

    exercicio 1557

---
+++ E1557/E1557.java
@@ -0,0 +1,25 @@
+package E1557;
+
+import java.util.Scanner;
+
+public class E1557 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int t = scanner.nextInt();
+
+            if (t == 0) {
+                break;
+            }
+
+            int[][] matriz = new int[t][t];
+
+            for (int i = 0; i < t; ++i) {
+                for (int j = 0; j < t; ++j) {
+                    System.out.println(matriz[i][i]);
+                }
+            }
+        }
+    }
+}
