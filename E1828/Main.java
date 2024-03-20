a25ccc6c5fbd303e84374a074dd6ef2f34ba17d0
Everton Santos <everton_projeto@outlook.com>
2024-03-20 15:36:51 -0300

    exercicio 1828

--- E1828/Main.java
+++ E1828/Main.java
@@ -2,7 +2,7 @@ package E1828;
 
 import java.util.Scanner;
 
-public class E1828 {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
