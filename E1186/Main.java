b13e00bd307a5f12dce2c970c3e68ec4b9a2e992
Everton Santos <everton_projeto@outlook.com>
2024-03-20 15:24:49 -0300

    exercicio 1186

--- E1186/Main.java
+++ E1186/Main.java
@@ -1,4 +1,36 @@
 package E1186;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[][] matriz = new double[12][12];
+        double soma = 0.0f;
+        String operacao = scanner.nextLine();
+
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 12; ++j) {
+                matriz[i][j] = scanner.nextDouble();
+            }
+        }
+
+        int contador = 0;
+
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                if (i + j > 11) {
+                    soma += matriz[i][j];
+                    contador++;
+                }
+            }
+        }
+
+        if (operacao.equals("S")) {
+            System.out.printf("%.1f\n", soma);
+        } else if (operacao.equals("M")) {
+            System.out.printf("%.1f\n", soma / contador);
+        }
+
+    }
 }
