30cb8e0ef9cecf76975ef7cd9806e69483b66f19
Everton Santos <everton_projeto@outlook.com>
2024-03-20 15:23:53 -0300

    exercicio 1864

---
+++ E1864/Main.java
@@ -0,0 +1,9 @@
+package E1864;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("LIFE IS NOT A PROBLEM TO BE SOLVED".substring(0, new Scanner(System.in).nextInt()));
+    }
+}
