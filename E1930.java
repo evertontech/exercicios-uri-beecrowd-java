87efa7143e94be983bd192db39a6dfe0b9b6b2c4
Everton Santos <everton_projeto@outlook.com>
2024-03-20 15:23:29 -0300

    exercicio 1930

---
+++ E1930.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class E1930 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t1 = scanner.nextInt();
+        int t2 = scanner.nextInt();
+        int t3 = scanner.nextInt();
+        int t4 = scanner.nextInt();
+        int total = t1 + t2 + t3 + t4 - 3;
+
+        System.out.println(total);
+
+    }
+}
