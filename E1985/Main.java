5e058c94925775544c0b26663efd13acefea00f8
Everton Santos <everton_projeto@outlook.com>
2024-03-23 05:03:27 -0300

    exercicio 1985

---
+++ E1985/Main.java
@@ -0,0 +1,40 @@
+package E1985;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int quantidadeDeProdutosComprados = scanner.nextInt();
+        double precoTotal = 0;
+
+        for (int i = 0; i < quantidadeDeProdutosComprados; ++i) {
+            int codigoDoProduto = scanner.nextInt();
+            int quantidade = scanner.nextInt();
+
+            switch (codigoDoProduto) {
+                case 1001:
+                    precoTotal += 1.50 * quantidade;
+                    break;
+
+                case 1002:
+                    precoTotal += 2.50 * quantidade;
+                    break;
+
+                case 1003:
+                    precoTotal += 3.50 * quantidade;
+                    break;
+
+                case 1004:
+                    precoTotal += 4.50 * quantidade;
+                    break;
+
+                case 1005:
+                    precoTotal += 5.50 * quantidade;
+                    break;
+            }
+        }
+
+       System.out.printf("%.2f\n", precoTotal);
+    }
+}
