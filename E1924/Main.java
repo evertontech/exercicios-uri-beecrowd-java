2495ed382132359c03167b5135d459463d68f00d
Everton Santos <everton_projeto@outlook.com>
2024-03-24 01:14:37 -0300

    exercicio 1924

---
+++ E1924/Main.java
@@ -0,0 +1,17 @@
+package E1924;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; ++i) {
+            String nomeDoCurso = scanner.nextLine();
+
+        }
+
+        System.out.println("Ciencia da Computacao");
+    }
+}
