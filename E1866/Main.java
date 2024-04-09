42401d732a79997676538c1f7cb666a628770f2d
Everton Santos <everton_projeto@outlook.com>
2024-04-09 14:04:53 -0300

    exercicio 1866

--- E1866/Main.java
+++ E1866/Main.java
@@ -6,22 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numeroDeCasosC = scanner.nextInt();
-        int n;
 
         for (int i = 0; i < numeroDeCasosC; ++i) {
-            n = scanner.nextInt();
-            int contador = 0;
+            int n = scanner.nextInt();
 
-            for (int j = 1; j <= n; ++j) {
-                if (j % 2 == 0) {
-                    contador--;
-                } else {
-                    contador++;
-                }
-
+            if (n % 2 == 0) {
+                System.out.println(0);
+            } else {
+                System.out.println(1);
             }
-
-            System.out.println(contador);
         }
     }
 }
