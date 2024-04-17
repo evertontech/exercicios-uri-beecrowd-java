10da657bda91b10bd518c1e755a2d392dac84643
Everton Santos <everton_projeto@outlook.com>
2024-04-17 14:27:17 -0300

    exercicio 1914

--- E1914/Main.java
+++ E1914/Main.java
@@ -8,22 +8,30 @@ public class Main {
         int quantidadeDeTestes = scanner.nextInt();
 
         for (int i = 0; i < quantidadeDeTestes; ++i) {
-            String nome1 = scanner.nextLine();
-            String jogada1 = scanner.nextLine();
-            String nome2 = scanner.nextLine();
-            String jogada2 = scanner.nextLine();
+            scanner.nextLine();
+            String[] entradas = scanner.nextLine().split(" ");
+            int numeroJogador1 = scanner.nextInt();
+            int numeroJogador2 = scanner.nextInt();
 
-            int numero1 = scanner.nextInt();
-            int numero2 = scanner.nextInt();
+            String nomeJogador1 = entradas[0];
+            String apostaJogador1 = entradas[1];
+            String nomeJogador2 = entradas[2];
 
-            int soma = numero1 + numero2;
+            int soma = numeroJogador1 + numeroJogador2;
 
             if (soma % 2 == 0) {
-                System.out.println(nome1);
+                if (apostaJogador1.equals("PAR")) {
+                    System.out.println(nomeJogador1);
+                } else {
+                    System.out.println(nomeJogador2);
+                }
             } else {
-                System.out.println(nome2);
+                if (apostaJogador1.equals("IMPAR")) {
+                    System.out.println(nomeJogador1);
+                } else {
+                    System.out.println(nomeJogador2);
+                }
             }
         }
-
     }
 }
