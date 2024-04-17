afcf9c8bbd53c4c6d167a949364e3d34e550bf36
Everton Santos <everton_projeto@outlook.com>
2024-04-17 14:26:24 -0300

    exercicio 1828

--- E1828/Main.java
+++ E1828/Main.java
@@ -14,31 +14,61 @@ public class Main {
             String jokenPoh1 = entrada[0];
             String jokenPoh2 = entrada[1];
 
-            switch(jokenPoh1) {
-                case "Spock": switch(jokenPoh2) {
-                    case "tesoura", "pedra": System.out.println("Caso #" + i + ": Bazinga!"); break;
-                    case "lagarto", "papel": System.out.println("Caso #" + i + ": Raj trapaceou!"); break;
-                } break;
+            switch (jokenPoh1) {
+                case "Spock":
+                    switch (jokenPoh2) {
+                        case "tesoura", "pedra":
+                            System.out.println("Caso #" + i + ": Bazinga!");
+                            break;
+                        case "lagarto", "papel":
+                            System.out.println("Caso #" + i + ": Raj trapaceou!");
+                            break;
+                    }
+                    break;
 
-                case "tesoura": switch(jokenPoh2) {
-                    case "papel", "lagarto": System.out.println("Caso #" + i + ": Bazinga!"); break;
-                    case "Spock", "pedra": System.out.println("Caso #" + i + ": Raj trapaceou!"); break;
-                } break;
+                case "tesoura":
+                    switch (jokenPoh2) {
+                        case "papel", "lagarto":
+                            System.out.println("Caso #" + i + ": Bazinga!");
+                            break;
+                        case "Spock", "pedra":
+                            System.out.println("Caso #" + i + ": Raj trapaceou!");
+                            break;
+                    }
+                    break;
 
-                case "pedra": switch(jokenPoh2) {
-                    case "lagarto", "tesoura": System.out.println("Caso #" + i + ": Bazinga!"); break;
-                    case "papel", "Spock": System.out.println("Caso #" + i + ": Raj trapaceou!"); break;
-                } break;
+                case "pedra":
+                    switch (jokenPoh2) {
+                        case "lagarto", "tesoura":
+                            System.out.println("Caso #" + i + ": Bazinga!");
+                            break;
+                        case "papel", "Spock":
+                            System.out.println("Caso #" + i + ": Raj trapaceou!");
+                            break;
+                    }
+                    break;
 
-                case "lagarto": switch(jokenPoh2) {
-                    case "Spock", "papel": System.out.println("Caso #" + i + ": Bazinga!"); break;
-                    case "pedra", "tesoura": System.out.println("Caso #" + i + ": Raj trapaceou!"); break;
-                } break;
+                case "lagarto":
+                    switch (jokenPoh2) {
+                        case "Spock", "papel":
+                            System.out.println("Caso #" + i + ": Bazinga!");
+                            break;
+                        case "pedra", "tesoura":
+                            System.out.println("Caso #" + i + ": Raj trapaceou!");
+                            break;
+                    }
+                    break;
 
-                case "papel": switch(jokenPoh2) {
-                    case "Spock", "pedra": System.out.println("Caso #" + i + ": Bazinga!"); break;
-                    case "tesoura", "lagarto": System.out.println("Caso #" + i + ": Raj trapaceou!"); break;
-                } break;
+                case "papel":
+                    switch (jokenPoh2) {
+                        case "Spock", "pedra":
+                            System.out.println("Caso #" + i + ": Bazinga!");
+                            break;
+                        case "tesoura", "lagarto":
+                            System.out.println("Caso #" + i + ": Raj trapaceou!");
+                            break;
+                    }
+                    break;
             }
 
             if (jokenPoh1.equals(jokenPoh2)) {
