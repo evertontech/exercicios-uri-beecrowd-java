42cebb00b875cd05e97ebe328d951c5c70af1067
Everton Santos <everton_projeto@outlook.com>
2024-04-18 15:34:27 -0300

    exercicio 2006

--- E2006/Main.java
+++ E2006/Main.java
@@ -5,34 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        int e = scanner.nextInt();
+        String t = scanner.nextLine();
+        String[] entradas = scanner.nextLine().split(" ");
+
         int contador = 0;
 
-        if (t == a) {
-            contador++;
-        }
-
-        if (t == b) {
-            contador++;
-        }
-
-        if (t == c) {
-            contador++;
-        }
-
-        if (t == d) {
-            contador++;
-        }
-
-        if (t == e) {
-            contador++;
+        for (String entrada : entradas) {
+            if (t.equals(entrada)) {
+                contador++;
+            }
         }
 
         System.out.println(contador);
+
     }
 }
