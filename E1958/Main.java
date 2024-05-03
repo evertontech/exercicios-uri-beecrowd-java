4ab3c088e5b88d472bee01af652938fee8654276
Everton Santos <everton_projeto@outlook.com>
2024-05-03 14:31:03 -0300

    exercicio 1958

--- E1958/Main.java
+++ E1958/Main.java
@@ -1,43 +1,30 @@
 package E1958;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String entrada = scanner.nextLine();
-        String[] vetor = entrada.split("\\.");
 
-        if (entrada.charAt(0) != '-') {
+        BigDecimal numero = new BigDecimal(entrada);
 
-            int expoente = vetor[0].length() - 1;
-            String eResposta = String.format("%02d", expoente);
+        String sinal = entrada.charAt(0) != '-' ? "+" : "-";
 
-            entrada = entrada.replace(".", "");
-            String novoValorString = entrada.charAt(0) + "." + entrada.substring(1, 6);
+        numero = numero.abs();
 
-            Double novoValor = Double.parseDouble(novoValorString);
+        int expoente = numero.precision() - numero.scale() - 1;
 
-            String valor = String.format("%.4f", novoValor).replace(",", ".");
+        BigDecimal mantissa = numero.movePointLeft(expoente);
 
-            String resposta = "+" + valor + "E+" + eResposta;
+        mantissa = mantissa.setScale(4, RoundingMode.HALF_UP);
 
-            System.out.println(resposta);
+        String resultado = String.format("%s%sE%+03d", sinal, mantissa.toPlainString(), expoente);
 
-        } else { // -0.000027
-            entrada = entrada.replace("-", "");
+        System.out.println(resultado);
 
-            int expoente = entrada.split("\\.")[1].length() - 1;
-            String expoenteString = String.format("%02d", entrada);
-
-            entrada = entrada.replace(".", "");
-
-            int mantissa = Integer.parseInt(entrada);
-
-            entrada = entrada.charAt(0) + "." + entrada.substring(1, 7); // 0.000027 , 1.1
-
-            System.out.println(entrada);
-
-        }
     }
 }
+
